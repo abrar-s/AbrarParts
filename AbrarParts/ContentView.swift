a245a9396587a28ca933826b0d94a9abe6f7f0a0
Abrar <abrar-s-95@hotmail.com>
2020-04-13 15:30:18 +0300

    Initial Commit

---
+++ AbrarParts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AbrarParts
+//
+//  Created by MacBook on 4/13/20.
+//  Copyright © 2020 abrarabrarabrar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
